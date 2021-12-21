0c4bfeb5108f6c02d7f2747fcba5b52271daa1bf
Andrey Belyaev <ianbel263@gmail.com>
2021-12-21 18:31:38 +0300

    fix bugs

--- market_prj/urls.py
+++ market_prj/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,3 +25,6 @@ urlpatterns = [
     path('', MainTemplateView.as_view(), name='main'),
     path('accommodations/', include('mainapp.urls', namespace='acc'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
